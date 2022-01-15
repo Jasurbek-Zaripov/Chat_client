1ab150525ab4af6d35664a46cd699ddbd09595bd
Jasurbek-Zaripov <zaripovjasurbek2001@gmail.com>
2022-01-15 11:50:48 +0500

    ishni boshlashga tayyorladim

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { join } from 'path'
+const app = express()
+
+app.use(express.static(join(process.cwd(), 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(join(process.cwd(), 'index.html'))
+})
+app.get('/:file', (req, res) => {
+  try {
+    if (['login', 'register'].includes(req.params.file)) {
+      return res.sendFile(join(process.cwd(), req.params.file + '.html'))
+    }
+    throw new Error('Not Found!')
+  } catch (xato) {
+    return res.status(404).json({ Error: xato.message })
+  }
+})
+
+app.listen(9080, () => console['log']('client: http://localhost:9080'))
